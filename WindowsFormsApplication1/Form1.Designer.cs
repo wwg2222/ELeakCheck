3906d945942c4c2fd52779c234b504f3ad9e8c27
rigid <WengangWang@WengangWang.IdealEnergy.Local>
2013-01-16 16:49:14 +0800

    Signed-off-by: rigid <WengangWang@WengangWang.IdealEnergy.Local>

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(59, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SynRead int1 and dint1，dint2\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(59, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 27);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "SynWrite int1 and dint1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(59, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 27);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "ASynRead int2 and sint1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(59, 208);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 27);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "ASynWrite int2 and sint1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(346, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(493, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(395, 37);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(104, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "opc1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(346, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(77, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(493, 170);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(80, 20);
+            this.textBox5.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "int1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(442, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "dint1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "int2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(442, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "sint1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(161, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(311, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Topic Name";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(493, 109);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(80, 20);
+            this.textBox6.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(442, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "dint2";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 268);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OPCAutomation;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string strTopic;
+        OPCServer MyServer;
+        OPCGroups MyGroups;
+        OPCGroup MyGroup;
+        OPCGroup MyGroup2;
+        OPCItems MyItems;
+        OPCItems MyItems2;
+        OPCItem[] MyItem;
+        OPCItem[] MyItem2;
+        string strHostIP = "";
+        string strHostName = "";
+        bool opc_connected = false;
+        int itmHandleClient = 0;
+        int itmHandleServer = 0;
+
+        private void GetLocalServer()
+        {
+            //获取本地计算机IP,计算机名称
+            strHostName = System.Net.Dns.GetHostName();
+            //或者通过局域网内计算机名称
+
+
+            //获取本地计算机上的OPCServerName
+            try
+            {
+                MyServer = new OPCServer();
+                object serverList = MyServer.GetOPCServers(strHostName);
+
+                foreach (string server in (Array)serverList)
+                {
+                    //cmbServerName.Items.Add(turn);
+                    Console.WriteLine("本地OPC服务器：{0}", server);
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("枚举本地OPC服务器出错：{0}", err.Message);
+            }
+        }
+
+        private bool ConnectRemoteServer(string remoteServerIP, string remoteServerName)
+        {
+            try
+            {
+                MyServer.Connect(remoteServerName, remoteServerIP);//连接本地服务器：服务器名+主机名或IP
+
+                if (MyServer.ServerState == (int)OPCServerState.OPCRunning)
+                {
+                    //                    string str1;
+
+                    label6.Text = "已连接到 RSLinx OPC Server";
+                //  timer1.Start();
+                    // Console.WriteLine("已连接到：{0}", MyServer.ServerName);
+                }
+                else
+                {
+                    //这里你可以根据返回的状态来自定义显示信息，请查看自动化接口API文档
+                    Console.WriteLine("状态：{0}", MyServer.ServerState.ToString());
+                    timer1.Stop();
+                }
+                MyServer.ServerShutDown += ServerShutDown;//服务器断开事件
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("连接远程服务器出现错误：{0}" + err.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public void ServerShutDown(string Reason)//服务器先行断开
+        {
+            Console.WriteLine("服务器已经先行断开！");
+        }
+
+        private bool CreateGroup()
+        {
+            try
+            {
+                MyGroups = MyServer.OPCGroups;
+                MyGroup = MyServer.OPCGroups.Add("测试");//添加组
+                MyGroup2 = MyServer.OPCGroups.Add("测试2");
+
+                //MyGroup2 = MyGroups.Add("测试2");
+                //以下设置组属性
+                {
+                    MyServer.OPCGroups.DefaultGroupIsActive = true;//激活组。
+                    MyServer.OPCGroups.DefaultGroupDeadband = 0;// 死区值，设为0时，服务器端该组内任何数据变化都通知组。
+                    MyServer.OPCGroups.DefaultGroupUpdateRate = 200;//默认组群的刷新频率为200ms
+                    MyGroup.UpdateRate = 100;//刷新频率为1秒。
+                    MyGroup.IsSubscribed = true;//禁用订阅功能，即可以异步，默认false
+
+                }
+
+                MyGroup.DataChange += new DIOPCGroupEvent_DataChangeEventHandler(GroupDataChange);
+                MyGroup.AsyncWriteComplete += new DIOPCGroupEvent_AsyncWriteCompleteEventHandler(GroupAsyncWriteComplete);
+                MyGroup.AsyncReadComplete += new DIOPCGroupEvent_AsyncReadCompleteEventHandler(GroupAsyncReadComplete);
+                MyGroup.AsyncWriteComplete += new DIOPCGroupEvent_AsyncWriteCompleteEventHandler(GroupAsyncWriteComplete);
+
+                {
+                    MyServer.OPCGroups.DefaultGroupIsActive = true;//激活组。
+                    MyServer.OPCGroups.DefaultGroupDeadband = 0;// 死区值，设为0时，服务器端该组内任何数据变化都通知组。
+                    MyServer.OPCGroups.DefaultGroupUpdateRate = 200;//默认组群的刷新频率为200ms
+                    MyGroup2.UpdateRate = 100;//刷新频率为1秒。
+                    MyGroup2.IsSubscribed = true;//使用订阅功能，即可以异步，默认false
+
+                }
+
+                MyGroup2.DataChange += new DIOPCGroupEvent_DataChangeEventHandler(GroupDataChange);
+                MyGroup2.AsyncWriteComplete += new DIOPCGroupEvent_AsyncWriteCompleteEventHandler(GroupAsyncWriteComplete);
+                MyGroup2.AsyncReadComplete += new DIOPCGroupEvent_AsyncReadCompleteEventHandler(GroupAsyncReadComplete);
+                MyGroup2.AsyncWriteComplete += new DIOPCGroupEvent_AsyncWriteCompleteEventHandler(GroupAsyncWriteComplete);
+
+                AddGroupItems();//设置组内items         
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("创建组出现错误：{0}", err.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private void AddGroupItems()//添加数据项
+        {
+            //itmHandleServer;
+            MyItems = MyGroup.OPCItems;
+            MyItems2 = MyGroup2.OPCItems;
+
+            //添加item
+            MyItem[0] = MyItems.AddItem("[" + strTopic + "]int1", 0);//[opc1]int1", 0);//int
+            MyItem[1] = MyItems.AddItem("[" + strTopic + "]dint1", 1);//dint
+            MyItem[2] = MyItems.AddItem("[" + strTopic + "]dint2", 1);//dint
+
+
+            MyItem2[0] = MyItems2.AddItem("[" + strTopic + "]int2", 0);//int
+            MyItem2[1] = MyItems2.AddItem("[" + strTopic + "]sint1", 1);//byte
+
+        }
+
+        void GroupDataChange(int TransactionID, int NumItems, ref Array ClientHandles, ref Array ItemValues, ref Array Qualities, ref Array TimeStamps)
+        {
+            Console.WriteLine("++++++++++++++++DataChanged+++++++++++++++++++++++");
+
+        }
+        void GroupDataChange2(int TransactionID, int NumItems, ref Array ClientHandles, ref Array ItemValues, ref Array Qualities, ref Array TimeStamps)
+        {
+            Console.WriteLine("----------------------DataChanged2------------------");
+
+        }
+        void GroupAsyncWriteComplete(int TransactionID, int NumItems, ref Array ClientHandles, ref Array Errors)
+        {
+
+            label3.Text = "成功写";
+            /*for (int i = 1; i <= NumItems; i++)
+            {
+                Console.WriteLine("Tran：{0}   ClientHandles：{1}   Error：{2}", TransactionID.ToString(), ClientHandles.GetValue(i).ToString(), Errors.GetValue(i).ToString());
+            }*/
+        }
+        void GroupAsyncReadComplete(int TransactionID, int NumItems, ref System.Array ClientHandles, ref System.Array ItemValues, ref System.Array Qualities, ref System.Array TimeStamps, ref System.Array Errors)
+        {
+
+            textBox4.Text = Convert.ToString(ItemValues.GetValue(1));
+            textBox5.Text = Convert.ToString(ItemValues.GetValue(2));
+
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyItem = new OPCItem[3];
+            MyItem2 = new OPCItem[2];
+            strTopic = textBox3.Text;
+            GetLocalServer();
+            //ConnectRemoteServer("192.168.1.35", "KEPware.KEPServerEx.V4");//用IP的局域网
+            if (ConnectRemoteServer("", "RSLinx OPC Server"))//本机
+            {
+                CreateGroup();
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            object ItemValues; object Qualities; object TimeStamps;//同步读的临时变量：值、质量、时间戳
+            MyItem[0].Read(1, out ItemValues, out Qualities, out TimeStamps);//同步读，第一个参数只能为1或2
+            int q0 = Convert.ToInt16(ItemValues);//转换后获取item值
+            textBox1.Text = q0 + "";
+            MyItem[1].Read(1, out ItemValues, out Qualities, out TimeStamps);//同步读，第一个参数只能为1或2
+            int q1 = Convert.ToInt32(ItemValues);//转换后获取item值
+            textBox2.Text = q1 + "";
+            MyItem[2].Read(1, out ItemValues, out Qualities, out TimeStamps);//同步读，第一个参数只能为1或2
+            int q2 = Convert.ToInt32(ItemValues);//转换后获取item值
+            textBox6.Text = q2 + "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int int1, dint1, dint2;
+            int1 = Convert.ToInt16(textBox1.Text);
+            dint1 = Convert.ToInt32(textBox2.Text);
+            dint2 = Convert.ToInt32(textBox6.Text);
+
+            MyItem[0].Write(int1);
+            MyItem[1].Write(dint1);
+            MyItem[2].Write(dint2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] temp = new int[] { 0, MyItem2[0].ServerHandle, MyItem2[1].ServerHandle };
+
+            Array serverHandles = (Array)temp;
+
+            Array Errors;
+            int cancelID;
+
+            MyGroup.AsyncRead(2, ref serverHandles, out Errors, 1, out cancelID);//第一参数为item数量
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] temp = new int[] { 0, MyItem2[0].ServerHandle, MyItem2[1].ServerHandle };
+            Array serverHandles = (Array)temp;
+            object[] valueTemp = new object[3];
+            int int1, sint1;
+            int1 = Convert.ToInt16(textBox4.Text);
+            sint1 = Convert.ToSByte(textBox5.Text);
+
+            valueTemp[0] = "";
+            valueTemp[1] = int1;
+            valueTemp[2] = sint1;
+
+            Array values = (Array)valueTemp;
+            Array Errors;
+            int cancelID;
+            MyGroup.AsyncWrite(2, ref serverHandles, ref values, out Errors, 1, out cancelID);//第一参数为item数量
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+                object ItemValues; object Qualities; object TimeStamps;//同步读的临时变量：值、质量、时间戳
+                MyItem[0].Read(1, out ItemValues, out Qualities, out TimeStamps);//同步读，第一个参数只能为1或2
+                int q0 = Convert.ToInt16(ItemValues);//转换后获取item值
+                textBox1.Text = q0 + "";
+                MyItem[1].Read(1, out ItemValues, out Qualities, out TimeStamps);//同步读，第一个参数只能为1或2
+                int q1 = Convert.ToInt32(ItemValues);//转换后获取item值
+                textBox2.Text = q1 + "";
+                MyItem[2].Read(1, out ItemValues, out Qualities, out TimeStamps);//同步读，第一个参数只能为1或2
+                int q2 = Convert.ToInt32(ItemValues);//转换后获取item值
+                textBox6.Text = q2 + "";
+            
+            
+        }
+    }
+}
